c113b890a42c1f703569f82e72b321172c1c98f8
omutucat <57890334+omutucat@users.noreply.github.com>
2023-12-17 11:56:51 +0000

    fix abc332
    add trailing newline

--- abc332/a/Program.fsx
+++ abc332/a/Program.fsx
@@ -1,9 +1,14 @@
-let n, s, k = stdin.ReadLine().Split([|' '|]) |> Array.map int |> fun a -> a.[0], a.[1], a.[2]
+let n, s, k =
+    stdin.ReadLine().Split([| ' ' |])
+    |> Array.map int
+    |> fun a -> a.[0], a.[1], a.[2]
 
 let mutable ans = 0
 
 for _ in 1..n do
-    let p, q = stdin.ReadLine().Split([|' '|]) |> Array.map int |> fun a -> a.[0], a.[1]
+    let p, q =
+        stdin.ReadLine().Split([| ' ' |]) |> Array.map int |> (fun a -> a.[0], a.[1])
+
     ans <- ans + p * q
 
-printf "%d" (ans + if ans < s then k else 0)
+printf "%d\n" (ans + if ans < s then k else 0)
